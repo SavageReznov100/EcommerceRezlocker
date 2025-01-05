16f7c8b293791a177b367760cd404d88947d423f
Olaide Afolabi <oafolabi@icsl.lo>
2025-01-05 21:48:52 +0100

    Fixed Redirects

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  preview: {
+    headers: {
+      "cache-control": "public, max-age=600",
+    },
+  },
+});
